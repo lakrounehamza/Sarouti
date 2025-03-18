f348e9f379b8f60e0f3340f1bf7e03b46de0c05a
lakrounehamza <lakrounehamza10@gmail.com>
2025-03-18 07:36:24 +0100

    ajouter le register

--- app/Http/Controllers/API/UserAuthController.php
+++ app/Http/Controllers/API/UserAuthController.php
@@ -4,8 +4,30 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use  App\Models\User;
 
 class UserAuthController extends Controller
 {
-
+    public function register(Request $request)
+    {
+        $validateRequest  = $request->validate([
+            'name' => 'required',
+            'email' => 'required',
+            'password' => 'required',
+            'role' => 'required',
+            'phone' => 'required',
+            'photo' => 'required',
+        ]);
+        User::create([
+            "name" => $validateRequest['name'],
+            "email" => $validateRequest['email'],
+            "password" => $validateRequest['password'],
+            "role" => $validateRequest['role'],
+            "phone" => $validateRequest['phone'],
+            "photo" => $validateRequest['photo'],
+        ]);
+        return  response()->json(["message" => "register"]);
+    }
+    public function login() {}
+    public function logout() {}
 }
