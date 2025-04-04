e327e928351f3db84abf0662c017f511451e7922
lakrounehamza <lakrounehamza10@gmail.com>
2025-04-04 18:27:48 +0200

    feat(auth): enhance email verification service with error handling and refactor

--- app/Customs/Services/EmailVerificationService.php
+++ app/Customs/Services/EmailVerificationService.php
@@ -42,4 +42,68 @@ class EmailVerificationService
             }
         }
     }
+    public function verifyEmail($token)
+    {
+        $emailVerification = EmailVirification::where('token', $token)->first();
+        if (!$emailVerification) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Invalid token'
+            ], 400);
+        }
+        if ($emailVerification->email_expired_at < now()) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Token expired'
+            ], 400);
+        }
+        if ($emailVerification->email_verified_at) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Email already verified'
+            ], 400);
+        }
+
+        $user = User::where('email', $emailVerification->email)->first();
+        $user->update(['email_verified_at' => now()]);
+        $emailVerification->delete();
+        return response()->json([
+            'success' => true,
+            'message' => 'Email verified successfully'
+        ], 200);
+    }
+    public function verifyToken($token)
+    {
+        $emailVerification = EmailVirification::where('token', $token)->first();
+        if (!$emailVerification) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Invalid token'
+            ], 400);
+        }
+        if ($emailVerification->email_expired_at < now()) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Token expired'
+            ], 400);
+        }
+        return response()->json([
+            'success' => true,
+            'message' => 'Token is valid'
+        ], 200);
+    }
+    public function  checkEmailVerification($user)
+    {
+        $emailVerification = EmailVirification::where('email', $user->email)->first();
+        if ($emailVerification) {
+            return response()->json([
+                'success' => true,
+                'message' => 'Email already verified'
+            ], 200);
+        }
+        return response()->json([
+            'success' => false,
+            'message' => 'Email not verified'
+        ], 400);
+    }
 }
