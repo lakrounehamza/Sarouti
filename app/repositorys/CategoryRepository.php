2f841782b6cd104e72eff658eac3fc15bf474e19
lakrounehamza <lakrounehamza10@gmail.com>
2025-04-05 21:58:53 +0200

    refactor: modifier le methode updateCategory

--- app/repositorys/CategoryRepository.php
+++ app/repositorys/CategoryRepository.php
@@ -1,36 +1,40 @@
 <?php
 
 namespace App\Repositorys;
+
 use  app\Contracts\CategoryRepositoryInterface;
 use  App\Models\Category;
 use  App\Http\Requests\CreateCategoryRequest;
 use  App\Http\Requests\UpdateCategoryRequest;
+
 class CategoryRepository implements CategoryRepositoryInterface
 {
-   
+
     public function getAllCategories()
     {
         return Category::all();
     }
 
-   
-    public function getCategoryById( $categoryId)
+
+    public function getCategoryById($categoryId)
     {
         $category = Category::find($categoryId);
-        if($category)
-        return $category;
+        if ($category)
+            return $category;
     }
 
-  
+
     public function createCategory(CreateCategoryRequest $attributes)
     {
         return Category::create($attributes->all());
     }
 
 
-    public function updateCategory(Category $category, UpdateCategoryRequest $attributes)
+    public function updateCategory($categoryId, UpdateCategoryRequest $attributes)
     {
-        $category->update($attributes->all());
+        $category = Category::find($categoryId);
+        if ($category)
+            $category->update($attributes->all());
         return $category;
     }
 
@@ -39,5 +43,4 @@ class CategoryRepository implements CategoryRepositoryInterface
     {
         $category->delete();
     }
-
 }
