e86df6662b6217adf7181d83922d331bb703646a
lakrounehamza <lakrounehamza10@gmail.com>
2025-04-05 21:37:51 +0200

    refactor: modifier le type des attributs das Annonceinterface

--- app/contracts/AnnonceRepositoryInterface.php
+++ app/contracts/AnnonceRepositoryInterface.php
@@ -1,16 +1,15 @@
 <?php
 
 namespace App\Contracts;
-use App\Models\Annonce;
 use  App\Http\Requests\CreateAnnonceRequest;
 use  App\Http\Requests\UpdateAnnonceRequest;
 
 interface AnnonceRepositoryInterface
 {
 public function getAllAnnonce();
-public function getAnnonceById(Annonce  $annonce);
+public function getAnnonceById($annonceId);
 public function createAnnonce(CreateAnnonceRequest  $attributes);
-public function UpdateAnnonce(Annonce $annonce ,UpdateAnnonceRequest  $attributes);
-public function deleteAnnone(Annonce $annonce);
+public function UpdateAnnonce($annonceId ,UpdateAnnonceRequest  $attributes);
+public function deleteAnnone($annonceid);
 
 }
