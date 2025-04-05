1abad06dc851aacbdc352afba70b0e60ed89f3bc
lakrounehamza <lakrounehamza10@gmail.com>
2025-04-05 21:46:05 +0200

    refactor: modifier le attributes methode UpdateAnnonce

--- app/repositorys/AnnonceRepository.php
+++ app/repositorys/AnnonceRepository.php
@@ -1,39 +1,47 @@
 <?php
 
 namespace App\Repositorys;
+
 use  App\Models\Annonce;
 use  App\Http\Requests\CreateAnnonceRequest;
 use  App\Http\Requests\UpdateAnnonceRequest;
 use App\Models\Images_annonce;
+
 class AnnonceRepository
 {
-    public function getAllAnnonce(){
+    public function getAllAnnonce()
+    {
         $annonces = Annonce::all();
         return  $annonces;
     }
-    public function getAnnonceById($annonceId){
+    public function getAnnonceById($annonceId)
+    {
         $annonce = Annonce::find($annonceId);
-        if($annonce){
-        return  $annonce;
-
+        if ($annonce) {
+            return  $annonce;
         }
     }
-    public function createAnnonce(CreateAnnonceRequest  $attributes){
+    public function createAnnonce(CreateAnnonceRequest  $attributes)
+    {
         Annonce::create([
-           'title' => $attributes->title ,
-            'description' => $attributes->description ,
-            'price' => $attributes->price ,
-            'type' => $attributes->type ,
-            'ville' => $attributes->ville ,
-            'status' => $attributes->status ,
-            'seller_id' => $attributes->seller_id ,
-            'category_id' => $attributes->category_id ,
+            'title' => $attributes->title,
+            'description' => $attributes->description,
+            'price' => $attributes->price,
+            'type' => $attributes->type,
+            'ville' => $attributes->ville,
+            'status' => $attributes->status,
+            'seller_id' => $attributes->seller_id,
+            'category_id' => $attributes->category_id,
         ]);
     }
-    public function UpdateAnnonce(Annonce $annonce ,UpdateAnnonceRequest  $attributes){
-        $annonce->update($attributes->all());
+    public function UpdateAnnonce($annonceId, UpdateAnnonceRequest  $attributes)
+    {
+        $annonce = Annonce::find($annonceId);
+        if ($annonce)
+            $annonce->update($attributes->all());
     }
-    public function deleteAnnone(Annonce $annonce){
+    public function deleteAnnone(Annonce $annonce)
+    {
         $annonce->delete();
     }
 }
