22faf9ad0533a6d85fd8575482f26ecf3efd6795
lakrounehamza <lakrounehamza10@gmail.com>
2025-04-05 18:53:09 +0200

    fix: corriger le nom du request

--- app/Http/Requests/CreateImageRequest.php
+++ app/Http/Requests/CreateImageRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class CreateTmageRequest extends FormRequest
+class CreateImageRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
