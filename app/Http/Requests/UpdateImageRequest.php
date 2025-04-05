ee7ad7dbfd3d7028a7141ce64164e4215ab5797d
lakrounehamza <lakrounehamza10@gmail.com>
2025-04-05 18:50:56 +0200

    feat: implementer la request de modification d'images pour les annonces

--- app/Http/Requests/UpdateImageRequest.php
+++ app/Http/Requests/UpdateImageRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class CreateTmageRequest extends FormRequest
+class UpdateImageRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -22,8 +22,7 @@ class CreateTmageRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'image' => 'required',
-            'annonce_id' => 'required|exists:annonces,id',
+            'path' => 'required',
         ];
     }
 }
