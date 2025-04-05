06b809f7bf3e56fcd7ca6285488e3e285ff99d48
lakrounehamza <lakrounehamza10@gmail.com>
2025-04-05 18:14:57 +0200

    fix: corriger le nom du controller CategoryController

---
+++ app/Http/Controllers/API/CategoryController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Contracts\CategoryRepositoryInterface;
+use App\Http\Requests\CreateCategoryRequest;
+use App\Http\Requests\UpdateCategoryRequest;
+use App\Models\Category;
+
+class CateroryController extends Controller
+{
+    private $categoryRepository;
+    public function __construct(CategoryRepositoryInterface $categoryRepository)
+    {
+
+        $this->categoryRepository = $categoryRepository;
+    }
+    /**
+     * Display a listing of the resource.
+     */
+    public function index() {
+        $categories = $this->categoryRepository->getAllCategories();
+        return response()->json([
+            'success' => true,
+            'message' => 'Categories retrieved successfully',
+            'categories' => $categories
+        ], 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(CreateCategoryRequest $request)
+    {
+        $category = $this->categoryRepository->createCategory($request);
+        return response()->json([
+            'success' => true,
+            'message' => 'Category created successfully',
+            'category' => $category
+        ], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Category $category)
+    {
+        $category = $this->categoryRepository->getCategoryById($category);
+        return response()->json([
+            'success' => true,
+            'message' => 'Category retrieved successfully',
+            'category' => $category
+        ], 200);
+    }
+  
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateCategoryRequest $request, Category $category)
+    {
+        $category = $this->categoryRepository->updateCategory($category, $request);
+        return response()->json([
+            'success' => true,
+            'message' => 'Category updated successfully',
+            'category' => $category
+        ], 200);
+    }
+    
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Category $category)
+    {
+        $this->categoryRepository->deleteCategory($category);
+        return response()->json([
+            'success' => true,
+            'message' => 'Category deleted successfully'
+        ], 200);
+    }
+}
