d0ba9c5f176e5e8e502bfe91024d2618d566cbe3
lakrounehamza <lakrounehamza10@gmail.com>
2025-04-06 16:07:55 +0200

    feat: implemention  repository  pour les  comments

---
+++ app/repositorys/CommentRepository.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Repositorys;
+use App\Contracts\CommentRepositoryInterface;
+use App\Models\Comment;
+class CommentRepository implements CommentRepositoryInterface
+{
+
+    public function getCommentById($commentId)
+    {
+        $comment = Comment::find($commentId);
+        if ($comment)
+            return $comment;
+    }
+    
+}
