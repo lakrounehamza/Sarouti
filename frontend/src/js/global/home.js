92db740cccc54c48a76e9c22176a702fbecf1691
lakrounehamza <lakrounehamza10@gmail.com>
2025-04-16 19:59:42 +0200

    ajouter une partie dynamique à un bouton d'inscription

--- frontend/src/js/global/home.js
+++ frontend/src/js/global/home.js
@@ -3,6 +3,9 @@ const  button_profile_button = document.getElementById("button_profile_button");
 const  connextoin_popup = document.getElementById("connextoin_popup"); 
 const connexion_button = document.getElementById("connexion_button");
 const close_popup_connextion = document.getElementById("close-popup-connextion");
+const  Inscription_popup = document.getElementById("Inscription_popup");
+const inscription_button = document.getElementById("inscription_button");
+const  close_popup_inscription = document.getElementById("close-popup-inscription");
 button_profile_button.addEventListener("click", function (event) {
     event.preventDefault();
     if(profile_menu.classList.contains("hidden")){
@@ -25,5 +28,21 @@ close_popup_connextion.addEventListener("click", function (event) {
         connextoin_popup.classList.remove("hidden");
     }else{
         connextoin_popup.classList.add("hidden");
+    }       
+});
+inscription_button.addEventListener("click", function (event) {
+    event.preventDefault();
+    if(Inscription_popup.classList.contains("hidden")){
+        Inscription_popup.classList.remove("hidden");
+    }else{
+        Inscription_popup.classList.add("hidden");
+    }       
+});
+close_popup_inscription.addEventListener("click", function (event) {
+    event.preventDefault();
+    if(Inscription_popup.classList.contains("hidden")){
+        Inscription_popup.classList.remove("hidden");
+    }else{
+        Inscription_popup.classList.add("hidden");
     }       
 });
